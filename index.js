79d8f9626c6321f7dd93dec7bdf41084df3d7ed8
uniquear <uniqueratliff@gmail.com>
2020-12-02 19:56:24 -0700

    addCate fixes

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ import { grantAuthToken, searchUserFromAuthToken } from "./auth";
 
 //Load database file upon first running the code
 export const dbPromise = open({
-  filename: "data.db",
-  driver: sqlite3.Database,
+    filename: "data.db",
+    driver: sqlite3.Database,
 });
 
 const app = express();
@@ -26,37 +26,38 @@ app.use("/static", express.static(__dirname + "/static"));
 app.use("/img", express.static(__dirname + "/img"));
 
 app.use(async (req, res, next) => {
-  const { userToken } = req.cookies;
-  if (!userToken) {
-    return next();
-  }
-  try {
-    const user = await searchUserFromAuthToken(userToken);
-    req.user = user;
-  } catch (e) {
-    return next(e);
-  }
-  next();
+    const { userToken } = req.cookies;
+    if (!userToken) {
+        return next();
+    }
+    try {
+        const user = await searchUserFromAuthToken(userToken);
+        req.user = user;
+    } catch (e) {
+        return next(e);
+    }
+    next();
 });
 
 // ==== HOME PAGE ====
 app.get("/", async (req, res) => {
-  //console.log("Display landing page");
-  res.render("home", { user: req.user });
+    //console.log("Display landing page");
+    res.render("home", { user: req.user });
 });
 
 // ==== ACCOUNT PAGE & ACTIONS ====
 app.get("/account", (req, res) => {
-  //console.log("Display account page");
-  res.render("account");
+    //console.log("Display account page");
+    res.render("account");
 });
 
 // --- register.handlebars actions ---
-    app.get("/register", (req, res) => {
+app.get("/register", (req, res) => {
     //console.log("Display register page");
     res.render("register");
-    });
-    app.post("/register", async (req, res) => {
+});
+
+app.post("/register", async (req, res) => {
     //console.log("Registering user...");
     const db = await dbPromise;
     const { username, email, password, password2 } = req.body;
@@ -65,37 +66,38 @@ app.get("/account", (req, res) => {
         const passwordHash = await bcrypt.hash(password, 10);
 
         try {
-        //insert users info into database
-        await db.run(
-            "INSERT INTO Users (username, email, password) VALUES (?, ?, ?);",
-            username,
-            email,
-            passwordHash
-        );
-        //keeps track of what user is registered in using cookies. also grants access to new user.
-        const user = await db.get("SELECT id FROM Users WHERE email=?", email);
-        const token = await grantAuthToken(user.id);
-        console.log("user registered", user);
-        res.cookie("userToken", token);
-        res.redirect("/");
+            //insert users info into database
+            await db.run(
+                "INSERT INTO Users (username, email, password) VALUES (?, ?, ?);",
+                username,
+                email,
+                passwordHash
+            );
+            //keeps track of what user is registered in using cookies. also grants access to new user.
+            const user = await db.get("SELECT id FROM Users WHERE email=?", email);
+            const token = await grantAuthToken(user.id);
+            console.log("user registered", user);
+            res.cookie("userToken", token);
+            res.redirect("/");
         } catch (e) {
-        return res.render("register", { error: e }, console.log(e));
+            return res.render("register", { error: e }, console.log(e));
         } //if something goes wrong during registration, error is passed.
     } else {
         res.render("register", {
-        error: "ERROR: Passwords did not match, please try again.",
+            error: "ERROR: Passwords did not match, please try again.",
         });
     }
-    });
+});
 
-    // --- login actions ---
-    app.get("/login", (req, res) => {
+// --- login actions ---
+app.get("/login", (req, res) => {
     if (req.user) {
         return res.redirect("/");
     }
     res.render("home");
-    });
-    app.post("/login", async (req, res) => {
+});
+
+app.post("/login", async (req, res) => {
     //console.log("Logging in user...");
     const db = await dbPromise;
     const { email, password } = req.body;
@@ -103,16 +105,16 @@ app.get("/account", (req, res) => {
     try {
         //checks users email if in the database
         const existingUser = await db.get(
-        "SELECT * FROM Users WHERE email=?",
-        email
+            "SELECT * FROM Users WHERE email=?",
+            email
         );
         if (!existingUser) {
-        throw "Incorrect Login Credentials: Please try again.";
+            throw "Incorrect Login Credentials: Please try again.";
         }
         //checks users password by decrpting hash from database
         const passwordMatch = await bcrypt.compare(password, existingUser.password);
         if (!passwordMatch) {
-        throw "Incorrect Login Credentials: Please try again.";
+            throw "Incorrect Login Credentials: Please try again.";
         }
 
         //if user has correct email and password it is grant access through grantAccess function in auth.js
@@ -122,27 +124,27 @@ app.get("/account", (req, res) => {
     } catch (e) {
         return res.render("home", { error: e });
     }
-    });
+});
 
-    // --- logout actions ---
-    app.get("/logout", (req, res) => {
+// --- logout actions ---
+app.get("/logout", (req, res) => {
     res.clearCookie("userToken");
     res.redirect("/");
-    });
+});
 
 // ==== ABOUT PAGE ====
 app.get("/about", (req, res) => {
-  res.render("about");
+    res.render("about");
 });
 
 // ==== TEAM BIO PAGE ====
 app.get("/team-bio", (req, res) => {
-  res.render("team-bio");
+    res.render("team-bio");
 });
 
 // ==== MOVIES & MOVIE-RELATED PAGES ====
-    // --- Movies.handlebars actions ---
-    app.get("/movies", async (req, res) => {
+// --- Movies.handlebars actions ---
+app.get("/movies", async (req, res) => {
     //console.log("Displaying Movies Page");
     const db = await dbPromise;
     const movies = await db.all(
@@ -155,26 +157,27 @@ app.get("/team-bio", (req, res) => {
                 FROM Movies`
     );
     res.render("movies", { movies });
-    });
-    app.post("/movies", async (req, res) => {
+});
+
+app.post("/movies", async (req, res) => {
     //console.log("Searching for Movie...");
     const db = await dbPromise;
     try {
         searchMovie = await db.get(
-        `SELECT
+            `SELECT
                     id
                 FROM Movies WHERE movieTitle=?`,
-        req.body.movieTitle
+            req.body.movieTitle
         );
         console.log("movie checked", searchMovie);
         res.redirect("/searchMovie");
     } catch (e) {
         return res.render("movies", { error: e });
     }
-    });
+});
 
-    // --- searchMovie.handlebars actions ---
-    app.get("/searchMovie", async (req, res) => {
+// --- searchMovie.handlebars actions ---
+app.get("/searchMovie", async (req, res) => {
     console.log("movie input", searchMovie);
     const db = await dbPromise;
     const movies = await db.get(
@@ -189,98 +192,99 @@ app.get("/team-bio", (req, res) => {
     );
     console.log("movie is", movies);
     res.render("searchMovie", { movies });
-    });
+});
 
-    // --- addmovie.handlebars actions ---
-    app.get("/addmovie", async (req, res) => {
+// --- addmovie.handlebars actions ---
+app.get("/addmovie", async (req, res) => {
     //console.log("Entered the addMovie Page")
     res.render("addmovie", { user: req.user });
-    });
-    app.post("/addmovie", async (req, res) => {
+});
+
+app.post("/addmovie", async (req, res) => {
     //console.log("Adding a movie right now...");
     const db = await dbPromise;
     try {
         //insert movies into the movies table in database.
         await db.run(
-        "INSERT INTO Movies (movieTitle, movieLength, movieYear, movieRating) VALUES (?, ?, ?, ?);",
-        req.body.movieTitle,
-        req.body.movieLength,
-        req.body.movieYear,
-        req.body.movieRating
+            "INSERT INTO Movies (movieTitle, movieLength, movieYear, movieRating) VALUES (?, ?, ?, ?);",
+            req.body.movieTitle,
+            req.body.movieLength,
+            req.body.movieYear,
+            req.body.movieRating
         );
         searchMovie = await db.get(
-        "SELECT id FROM Movies WHERE movieTitle=?",
-        req.body.movieTitle
+            "SELECT id FROM Movies WHERE movieTitle=?",
+            req.body.movieTitle
         );
         res.redirect("movieAdded");
     } catch (e) {
         return res.render("addmovie", {
-        error: "Movie already in database. Please try another movie.",
-        user: req.user,
+            error: "Movie already in database. Please try another movie.",
+            user: req.user,
         });
     }
-    });
+});
 
-    // --- movieAdded.handlebars actions ---
-    app.get("/movieAdded", async (req, res) => {
+// --- movieAdded.handlebars actions ---
+app.get("/movieAdded", async (req, res) => {
     //console.log("Added a movie!")
     const db = await dbPromise;
     if (searchMovie.id) {
         const movies = await db.get(
-        "SELECT movieTitle, movieLength, movieYear, movieRating FROM Movies WHERE id=?",
-        searchMovie.id
+            "SELECT movieTitle, movieLength, movieYear, movieRating FROM Movies WHERE id=?",
+            searchMovie.id
         );
         console.log("moive id added", movies);
         res.render("movieAdded", { movies });
     } else {
         res.render("movieAdded");
     }
-    });
+});
 
 // ==== CATEGORY & CATEGORY-RELATED PAGES ====
-    // --- categories.handlebars actions ---
-    app.get("/categories", (req, res) => {
+// --- categories.handlebars actions ---
+app.get("/categories", (req, res) => {
     res.render("categories");
-    });
+});
 
-    // --- addCate.handlebars actions ---
-    app.get("/addCate", async (req, res) => {
+// --- addCate.handlebars actions ---
+app.get("/addCate", async (req, res) => {
     //console.log("Display addCate page")
     res.render("addCate", { user: req.user });
-    });
-    app.post("/addCate", async (req, res) => {
-    //console.log("Adding a category...")
-    if (!req.user) {
-        return res.redirect("/");
-    }
+});
+
+app.post("/addCate", async (req, res) => {
+    console.log("Adding a category right now...");
     const db = await dbPromise;
-    movieID = await db.get(
-        "SELECT id FROM Movies WHERE MovieTitle=?",
-        searchMovie
-    );
-    console.log("movie ID", movieID);
     try {
-        const test = await db.run(
-        "INSERT INTO Category (addCategory, movieId) VALUES (?, ?);",
-        req.body.addCategory,
-        movieID.id
+        //insert category into the  category table in database.
+        await db.run(
+            "INSERT INTO Category (id, addCategory, movieId) VALUES (?, ?, ?);",
+            req.body.id,
+            req.body.addCategory,
+            req.body.moveId,
         );
-        console.log("test", movieID);
-        console.log("test 2", test);
+        searchCategory = await db.get(
+            "SELECT addCategory FROM Category WHERE addCategory=?",
+            req.body.cate-name
+        );
         res.redirect("/addCate");
     } catch (e) {
-        return res.render("addCate", { error: e, user: req.user });
+        return res.render("addmovie", {
+            error: "Category is already in database.",
+            user: req.user,
+        });
     }
-    });
+});
 
 //Gets access to database and runs migration
 const setup = async () => {
-  const db = await dbPromise;
-  await db.migrate();
+    const db = await dbPromise;
+    await db.migrate();
 
-  app.listen(8000, () => {
-    console.log("Listening on port 8080 @ http://localhost:8000");
-  });
+    app.listen(8000, () => {
+        console.log("Listening on port 8080 @ http://localhost:8000");
+    });
 };
 
 setup();
